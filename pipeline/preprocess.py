Fix scaling

pipeline/preprocess.py
@@ -2,6 +2,7 @@ import numpy as np
 import functools
 import operator
 import pandas as pd
+from sklearn.impute import KNNImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import ShuffleSplit
 
@@ -52,13 +53,23 @@ class DataPreprocessing:
         self.X_train, self.X_test = self.X.iloc[train_index, :], self.X.iloc[test_index, :]
         self.y_train, self.y_test = self.y.iloc[train_index, :], self.y.iloc[test_index, :]
         strains_training, strains_testing = self.strains.iloc[train_index, :], self.strains.iloc[test_index, :]
-        scaler = StandardScaler()
-        X_train = pd.DataFrame(scaler.fit_transform(self.X_train.values), columns=self.X_train.columns, index=self.X_train.index)
-        X_test = pd.DataFrame(scaler.fit_transform(self.X_test.values), columns=self.X_test.columns, index=self.X_test.index)
-        X_train_means = X_train.mean()
-        X_test_means = X_test.mean()
-        X_train = X_train.fillna(X_train_means)
-        X_test = X_test.fillna(X_test_means)
+
+       # knn_imputer = KNNImputer(n_neighbors=3)
+
+        # Fit and transform the training data
+       # self.X_train = pd.DataFrame(knn_imputer.fit_transform(self.X_train.values),columns=self.X_train.columns, index=self.X_train.index)
+       # self.X_test = pd.DataFrame(knn_imputer.fit_transform(self.X_test),columns=self.X_test.columns, index=self.X_test.index)
+
+       # scaler = StandardScaler().fit(self.X_train.values)
+       # X_train = pd.DataFrame(scaler.transform(self.X_train.values), columns=self.X_train.columns, index=self.X_train.index)
+      #  X_test = pd.DataFrame(scaler.transform(self.X_test.values), columns=self.X_test.columns, index=self.X_test.index)
+        X_train_means = self.X_train.mean()
+        X_test_means = self.X_test.mean()
+
+        X_train = self.X_train.fillna(X_train_means)
+        X_test = self.X_test.fillna(X_test_means)
+
+
         y_test = np.asarray(functools.reduce(operator.iconcat, np.asarray(self.y_test), []))
         y_train = np.asarray(functools.reduce(operator.iconcat, np.asarray(self.y_train), []))
         return {'X_train': X_train, 'y_train': y_train, 'X_test': X_test, 'y_test': y_test,