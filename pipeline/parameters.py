2f9646d8f987197c57bfaaf923064eb5741a4230
SakshiKhaiwal <sakshikhaiwal@gmail.com>
2025-02-19 15:21:28 +0100

    Add new params

--- pipeline/parameters.py
+++ pipeline/parameters.py
@@ -21,11 +21,12 @@ def get_parameters():
                         help='apply feature selection')
     parser.add_argument('--feature_selection_strategy', type=str, required=False, default='lasso_selection_grid',
                         choices=['lasso_selection_grid', 'lasso_selection_grid_optimized', 'lasso_selection_random',
-                                 'lasso_selection_bayes', 'high_lasso'],
+                                 'lasso_selection_bayes', 'Boruta_selection','high_lasso'],
                         help='choice of feature selection strategy')
     parser.add_argument('--model_type', type=str, default='RandHypOPt_Ridge_regression',
                         choices=['BayesHypOPt_Ridge_regression', 'BayesHypOPt_Elanet_regression',
-                                 'BayesHypOPt_GBM_regression', 'BayesHypOPt_SVR_regression',
+                                 'BayesHypOPt_GBM_regression','BayesHypOPt_HistGBM_regression',
+                                 'BayesHypOPt_SVR_regression',
                                  'BayesHypOPt_NN_regression', 'RandHypOPt_Ridge_regression',
                                  'RandHypOPt_Elanet_regression', 'RandHypOPt_GBM_regression',
                                  'RandHypOPt_SVR_regression', 'RandHypOPt_NN_regression'],
