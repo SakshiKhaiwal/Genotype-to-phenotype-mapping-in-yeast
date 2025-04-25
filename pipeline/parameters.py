fb17e8a092843bd40a6668419de8f183bd0cb001
SakshiKhaiwal <sakshikhaiwal@gmail.com>
2025-04-25 09:07:36 +0200

    Add model params

--- pipeline/parameters.py
+++ pipeline/parameters.py
@@ -14,7 +14,7 @@ def get_parameters():
     parser.add_argument('--data_path_out', type=str, required=True,
                         help='path to the output data')
     parser.add_argument('--clades_data_path', type=str, required=False, help='path to clades data')
-    parser.add_argument('--data_splitting_criteria', type=str, default='preprocess_data_HOAR',
+    parser.add_argument('--data_splitting_criteria', type=str, default='preprocess_data_HOAR_MI',
                         choices=['preprocess_data_HOAR_MI','preprocess_data_HOAR_KNN','preprocess_data_INHO', 'preprocess_data_LOCO'],
                         help='how to split the data into training and testing')
     parser.add_argument('--do_feature_selection', type=bool, required=False, default=False,
@@ -30,5 +30,11 @@ def get_parameters():
                                  'RandHypOPt_Ridge_regression','RandHypOPt_Elanet_regression',
                                  'RandHypOPt_GBM_regression','RandHypOPt_SVR_regression',
                                  'RandHypOPt_NN_regression'], help='used model')
+    parser.add_argument('--n_iterations', type=int, default=100,
+                        help='number of iterations for the model')
+    parser.add_argument('--cross_val', type=int, default=5,
+                        help='value of the cross validation')
+    parser.add_argument('--num_jobs', type=int, default=-1,
+                        help='number of jobs to use')
 
     return parser.parse_args()
